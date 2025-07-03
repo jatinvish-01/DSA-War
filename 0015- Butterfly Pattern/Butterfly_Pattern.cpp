5c3d2b4a2bf8f4891d0455b0d670efa4ba002f5f
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-04 00:39:58 +0530

    Butterfly Pattern

--- 0015- Butterfly Pattern/Butterfly_Pattern.cpp
+++ 0015- Butterfly Pattern/Butterfly_Pattern.cpp
@@ -4,19 +4,19 @@ using namespace std;
 void printBatterflyPattern(int n){
       for(int i = 0; i<2*n; i++){ // row
 
-            int cond = i < n ? i : 0;
-            int space_count = i < n ? 2*(n-cond-1) : 0;
+            int cond = i < n ? i : n+(n-i-1);
+            int space_count = i < n ? 2*(n-cond-1) : i-cond-1;
 
             for(int j = 0; j<2*n; j++){ // col
                   if(j<=cond){
-                        cout << "*";
+                        cout << "* ";
                   }
                   else if(space_count > 0){
-                        cout << " ";
+                        cout << "  ";
                         space_count--;
                   }
                   else{
-                        cout << "*";
+                        cout << "* ";
                   }
             }
             cout << endl;
