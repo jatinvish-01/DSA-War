a0cb28d04c5d9ff9b17cd556bab2d094fe6b41c0
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-07-03 23:12:04 +0530

    initial commit

---
+++ 0015- Butterfly Pattern/Butterfly_Pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void printBatterflyPattern(int n){
+      for(int i = 0; i<2*n; i++){
+            
+      }
+}
+
+int main(){
+      int n;
+      cin >> n;
+      printBatterflyPattern(n);
+}
